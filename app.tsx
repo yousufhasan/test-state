74a036baf4b66b45a22156e721bf493ce51cd0f2
Yousuf Hasan <yousuf85@gmail.com>
2023-11-23 12:40:03 +1100

    Update app.tsx

--- app.tsx
+++ app.tsx
@@ -6,26 +6,36 @@ import { createMachine } from 'xstate';
 import { ApplicantDetails } from './pages/ApplicantDetails/ApplicantDetails';
 import { applicantDetailsMachine } from './pages/ApplicantDetails/ApplicantDetails';
 
+import { createMachine, assign } from 'xstate';
+
+import { applicantDetailsMachine } from './pages/ApplicantDetails/ApplicantDetails';
+
 export const appMachine = createMachine({
   initial: 'applicantDetails',
   id: 'application',
+  invoke: {
+    id: 'applicantDetails',
+    src: applicantDetailsMachine,
+    onSnapshot: {
+      actions: assign(({ event }) => {
+        console.log(event, 'in snapshot');
+        return { applicantDetails: '' };
+      }),
+    },
+
+    onDone: {
+      actions: () => {
+        console.log('Received completed event!');
+      },
+    },
+  },
   states: {
     applicantDetails: {
-      invoke: {
-        id: 'applicantDetails',
-        src: applicantDetailsMachine,
-        /*  onDone: {
-          target: 'assetDetails',
-          actions: () => {
-            console.log('Received completed event!');
-          },
-        },*/
-      },
       on: {
         'applicationDetails.completed': {
           target: 'assetDetails',
-          actions: () => {
-            console.log('Received completed event!');
+          actions: args => {
+            console.log('Received completed event!', args);
           },
         },
       },
